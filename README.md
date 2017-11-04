f6461032bd9eae66ea5b7468cbfeebdcf2d079bc
Cygnan <email@cygnan.com>
2017-11-04 14:43:38 +0900

    Add Travis CI & Code Climate config

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # twitter-wordcloud-bot
 
+[![Build Status](https://travis-ci.org/cygnan/twitter-wordcloud-bot.svg?branch=master)](https://travis-ci.org/cygnan/twitter-wordcloud-bot)
+[![Maintainability](https://api.codeclimate.com/v1/badges/6302b1e5c142245d7d6a/maintainability)](https://codeclimate.com/github/cygnan/twitter-wordcloud-bot/maintainability)
+
 This script is currently under development.
--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 
 import os
 import logging
+import sys
 import tweepy
 
 logging.basicConfig()
@@ -13,12 +14,22 @@ CONSUMER_KEY = os.environ["CONSUMER_KEY"]
 CONSUMER_SECRET = os.environ["CONSUMER_SECRET"]
 ACCESS_TOKEN = os.environ["ACCESS_TOKEN"]
 ACCESS_TOKEN_SECRET = os.environ["ACCESS_TOKEN_SECRET"]
+is_travis = False
+
+logger.info("sys.argv: {}".format(sys.argv))
+logger.info("len(sys.argv): {}".format(len(sys.argv)))
+
+if len(sys.argv) == 2 and str(sys.argv[1]) == "--travis":
+    logger.info("Started Travis CI building test...")
+    is_travis = True
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
 
 class MyStreamListener(tweepy.StreamListener):
+
+
     def on_status(self, status):
         try:
             tweet_username = str(status.user.screen_name)
@@ -28,7 +39,7 @@ class MyStreamListener(tweepy.StreamListener):
             logger.info('@{}: "{}"'.format(tweet_to, tweet_text))
 
             if tweet_to == str(api.me().screen_name):
-                my_reply = "@" + tweet_username + " " + tweet_text #Test
+                my_reply = "@" + tweet_username + " " + tweet_text  # Test
 
                 api.update_status(status=my_reply)
 
@@ -44,12 +55,16 @@ class MyStreamListener(tweepy.StreamListener):
         logger.warning("Error")
         logger.error(status_code)
 
+
+logger.info("Hello @{}!".format(api.me().screen_name))
+
+my_stream_listener = MyStreamListener()
+my_stream = tweepy.Stream(auth=api.auth, listener=my_stream_listener)
+
+if is_travis is True:
+    sys.exit()
+
 try:
-    print("Hello @{}!".format(api.me().screen_name))
-
-    my_stream_listener = MyStreamListener()
-    my_stream = tweepy.Stream(auth=api.auth, listener=my_stream_listener)
-
     logger.info("Started streaming...")
 
     my_stream.userstream()
