851f0c563fe1cb2768d9b4ee9c3b56f47270daee
Cygnan <email@cygnan.com>
2017-11-04 14:35:52 +0900

    Follow PEP 8 style guide

--- main.py
+++ main.py
@@ -21,18 +21,18 @@ api = tweepy.API(auth)
 class MyStreamListener(tweepy.StreamListener):
     def on_status(self, status):
         try:
-            tweetUserName = str(status.user.screen_name)
-            tweetTo = str(status.in_reply_to_screen_name)
-            tweetText = str(status.text.encode('utf_8'))
+            tweet_username = str(status.user.screen_name)
+            tweet_to = str(status.in_reply_to_screen_name)
+            tweet_text = str(status.text.encode('utf_8'))
 
-            log.info("@{}: \"{}\"".format(tweetTo, tweetText))
+            log.info('@{}: "{}"'.format(tweet_to, tweet_text))
 
-            if tweetTo == str(api.me().screen_name):
-                reply = "@" + tweetUserName + " " + tweetText #Test
+            if tweet_to == str(api.me().screen_name):
+                my_reply = "@" + tweet_username + " " + tweet_text #Test
 
-                api.update_status(status=reply)
+                api.update_status(status=my_reply)
 
-                log.info("-> Tweeted \"{}\"".format(reply))
+                log.info('-> Tweeted "{}"'.format(my_reply))
             else:
                 log.info("-> Skipped.")
             return
@@ -47,12 +47,12 @@ class MyStreamListener(tweepy.StreamListener):
 try:
     print("Hello @{}!".format(api.me().screen_name))
 
-    myStreamListener = MyStreamListener()
-    myStream = tweepy.Stream(auth=api.auth, listener=myStreamListener)
+    my_stream_listener = MyStreamListener()
+    my_stream = tweepy.Stream(auth=api.auth, listener=my_stream_listener)
 
     log.info("Started streaming...")
 
-    myStream.userstream()
+    my_stream.userstream()
 
     log.info("Finished streaming.")
 
