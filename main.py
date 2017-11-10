3627ae651a8954a0e23b57d3b84bb6323228d290
Cygnan <email@cygnan.com>
2017-11-10 18:38:27 +0900

    Increase adjectives counts to 10000 times

--- main.py
+++ main.py
@@ -84,7 +84,7 @@ class MyStreamListener(tweepy.StreamListener):
                     word_type = node.feature.split(",")[0]
                     if word_type == "形容詞":
                       word = node.surface
-                      frequency[word] += 1000
+                      frequency[word] += 10000
                     elif word_type in ["動詞", "名詞", "副詞"]:
                       word = node.surface
                       frequency[word] += 1
