57f6cc07144d04268071aba5c60c53ec528380ce
Cygnan <email@cygnan.com>
2018-02-14 01:53:59 +0900

    Dislpay all logs (#84)

--- twitter-wordcloud-bot/logger.py
+++ twitter-wordcloud-bot/logger.py
@@ -2,10 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import logging
-import os
 
 logging.basicConfig(format=u"[%(filename)s:%(lineno)d] %(message)s")
-logger = logging.getLogger(
-    os.path.splitext(os.path.basename(__file__))[0].decode("utf-8")
-)
+logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
